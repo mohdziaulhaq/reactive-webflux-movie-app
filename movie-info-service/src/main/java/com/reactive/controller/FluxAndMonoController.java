d74e0a39aa089098cec8ce9b54c1f9520f1a7269
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-05-04 10:59:07 -0500

    added movie-info-service

---
+++ movie-info-service/src/main/java/com/reactive/controller/FluxAndMonoController.java
@@ -0,0 +1,24 @@
+package com.reactive.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FluxAndMonoController {
+
+    @GetMapping("/flux")
+    public Flux<Integer> flex(){
+        return Flux.just(1,2,3)
+                .log();
+    }
+
+    @GetMapping("/mono")
+    public Mono<String> helloWorldMono(){
+        return Mono.just("hello-world")
+                .log();
+    }
+
+
+}
