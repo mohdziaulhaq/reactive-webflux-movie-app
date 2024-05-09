dcb442f5f7a81b9946b064968c9eb307f204aec9
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-05-09 17:10:03 -0500

    added global error handler

--- movies-service/src/main/java/com/reactive/globalerrorhandler/GlobalErrorHandler.java
+++ movies-service/src/main/java/com/reactive/globalerrorhandler/GlobalErrorHandler.java
@@ -1,2 +1,18 @@
-package com.reactive;public class globalerrorhandler {
+package com.reactive.globalerrorhandler;
+
+import com.reactive.exception.MoviesInfoClientException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalErrorHandler {
+
+    @ExceptionHandler(MoviesInfoClientException.class)
+    public ResponseEntity<String> handleClientException(MoviesInfoClientException exception){
+        log.error("Exception caught in handleClientException :{}", exception.getMessage());
+        return ResponseEntity.status(exception.getStatusCode()).body(exception.getMessage());
+    }
 }
