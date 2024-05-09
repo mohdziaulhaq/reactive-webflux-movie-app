c5bee514fc219bc63fece0095926776b2283be02
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-05-09 17:11:04 -0500

    handle 4xx excpetion in MoviesInfoRestClient

--- movies-service/src/main/java/com/reactive/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactive/client/MoviesInfoRestClient.java
@@ -1,11 +1,16 @@
 package com.reactive.client;
 
 import com.reactive.domain.MovieInfo;
+import com.reactive.exception.MoviesInfoClientException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 public class MoviesInfoRestClient {
 
@@ -24,8 +29,19 @@ public class MoviesInfoRestClient {
                 .get()
                 .uri(url,movieId)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
+                    log.info("Status code is : {}", clientResponse.statusCode().value());
+                    if(clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)){
+                        return Mono.error(new MoviesInfoClientException("There is no MovieInfo available for the passed in Id :"+movieId
+                                ,clientResponse.statusCode().value()));
+
+                    }
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage -> Mono.error(new MoviesInfoClientException(
+                                    responseMessage, clientResponse.statusCode().value())));
+                })
                 .bodyToMono(MovieInfo.class)
                 .log();
-        //added comment to reload
+
     }
 }
